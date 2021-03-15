88b55ba3f97e0d3f487171091e5d357903c84cd3
DarkJoker360 <simoespo159@gmail.com>
2021-03-15 13:04:07 +0100

    Updater: Switch to own builds.
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- src/org/pixelexperience/ota/misc/Constants.java
+++ src/org/pixelexperience/ota/misc/Constants.java
@@ -30,9 +30,9 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://download.pixelexperience.org/ota_v5/%s/%s";
-    static final String OTA_CI_URL = "https://download.pixelexperience.org/ota_ci/%s/%s";
-    static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
-    static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
+    static final String OTA_URL = "https://raw.githubusercontent.com/pixel-ginkgo/OTA/eleven/ginkgo.json";
+    static final String OTA_CI_URL = "";
+    static final String MAINTAINER_URL = "https://t.me/ginkgodev";
+    static final String DOWNLOAD_WEBPAGE_URL = "https://darkjoker360.github.io/2021/PE-11-ginkgo/";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
